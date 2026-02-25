cfdd71a32aa46e86abca3efaf100437230bafe1d
hetfield74 <hetfield@merz-it-service.de>
2026-02-25 15:32:19 +0100

    small changes

--- shoproot/admin/includes/extra/footer/mits_copy_option_with_values.php
+++ shoproot/admin/includes/extra/footer/mits_copy_option_with_values.php
@@ -12,16 +12,19 @@
  * --------------------------------------------------------------
  */
 
-if (basename($PHP_SELF) == FILENAME_PRODUCTS_ATTRIBUTES && is_object($oInfo)) {
-  $MITS_CopyOptionAndValues = '<table class="contentTable"><tbody><tr class="infoBoxHeading"><td class="infoBoxHeading" style="text-align:center"><a class="button" id="copy_option_and_values" onclick="this.blur();" href="' . xtc_href_link(FILENAME_PRODUCTS_ATTRIBUTES, xtc_get_all_get_params(array('action', 'search', 'oID')) . 'action=copy_option_and_values&oID=' . $oInfo->products_options_id) . '">Option inkl. der Werte kopieren</a></td></tr></tbody></table>';
-  ?>
+if (isset($PHP_SELF) && defined('FILENAME_PRODUCTS_ATTRIBUTES') && basename($PHP_SELF) == FILENAME_PRODUCTS_ATTRIBUTES && isset($oInfo) && is_object($oInfo)) {
+    $MITS_CopyOptionAndValues = '<table class="contentTable"><tbody><tr class="infoBoxHeading"><td class="infoBoxHeading" style="text-align:center"><a class="button" id="copy_option_and_values" onclick="this.blur();" href="' . xtc_href_link(FILENAME_PRODUCTS_ATTRIBUTES, xtc_get_all_get_params(array('action', 'search', 'oID')) . 'action=copy_option_and_values&oID=' . $oInfo->products_options_id) . '">' . MITS_COPY_OPTION_WITH_VALUES_TEXT_BUTTON . '</a></td></tr></tbody></table>';
+    ?>
   <script>
     $(document).ready(function () {
       $('.boxRight .contentTable:last').after('<?php echo $MITS_CopyOptionAndValues;?>');
     });
   </script>
   <style>
-    #copy_option_and_values{text-align:center;margin:0 auto}
+    #copy_option_and_values {
+      text-align: center;
+      margin: 0 auto
+    }
   </style>
-  <?php
+    <?php
 }
